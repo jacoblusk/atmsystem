8ae645fe557e642db679a2d74f517b3cf2a10edd
jacoblusk <jacobplusk@gmail.com>
2015-02-26 17:21:34 -0800

    make bank methods comform to rpc standards

--- bank.go
+++ bank.go
@@ -8,25 +8,49 @@ type Bank struct {
 	storage Storage
 }
 
-func (b *Bank) Deposit(id, amount int) error {
-	a, err := b.storage.GetAccount(id)
+type Transaction struct {
+	ID, Amount int
+}
+
+func (b *Bank) Deposit(t, r *Transaction) error {
+	a, err := b.storage.GetAccount(t.ID)
 	if err != nil {
 		return err
 	}
 
-	err = b.storage.UpdateBalance(id, a.Balance+amount)
+	newBalance := a.Balance + t.Amount
+
+	r.ID = a.ID
+	r.Amount = newBalance
+
+	err = b.storage.UpdateBalance(a.ID, newBalance)
 	return err
 }
 
-func (b *Bank) Withdrawl(id, amount int) error {
+func (b *Bank) Withdrawl(t, r *Transaction) error {
+	a, err := b.storage.GetAccount(t.ID)
+	if err != nil {
+		return err
+	}
+
+	if t.Amount > a.Balance {
+		return errors.New("Balance exceeded")
+	}
+
+	newBalance := a.Balance - t.Amount
+	r.ID = a.ID
+	r.Amount = newBalance
+	err = b.storage.UpdateBalance(a.ID, newBalance)
+	return err
+}
+
+func (b *Bank) Inquiry(id int, r *Transaction) error {
 	a, err := b.storage.GetAccount(id)
 	if err != nil {
 		return err
 	}
 
-	if amount > a.Balance {
-		return errors.New("Balance exceeded")
-	}
-	err = b.storage.UpdateBalance(id, a.Balance+amount)
+	r.ID = a.ID
+	r.Amount = a.Balance
 	return err
 }
