ba5945f6e88e5a0bdaea5c20f9c248f7f144c3e2
jacoblusk <jacobplusk@gmail.com>
2015-03-02 16:43:07 -0800

    change storage interface

--- storage.go
+++ storage.go
@@ -1,8 +1,6 @@
 package atmsystem
 
 type Storage interface {
-	Open(filename string) error
-	Close() error
 	PutAccount(account *Account) error
 	GetAccount(id int) (*Account, error)
 	Exists(id int) (bool, error)
